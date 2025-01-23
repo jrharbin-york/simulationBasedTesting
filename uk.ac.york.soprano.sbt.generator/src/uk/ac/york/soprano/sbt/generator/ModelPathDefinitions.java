8f8cd391d45ff662c82d90c16da4794060548a0b
James Harbin <james.harbin@york.ac.uk>
2025-01-23 13:52:05 +0000

    Fixes for the model path definitions

---
+++ uk.ac.york.soprano.sbt.generator/src/uk/ac/york/soprano/sbt/generator/ModelPathDefinitions.java
@@ -0,0 +1,37 @@
+package uk.ac.york.soprano.sbt.generator;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ModelPathDefinitions {
+
+        private static Properties loadProperties() throws MissingPropertiesFile {
+                String homeDir = System.getProperty("user.home");
+                File propsFile = new File(homeDir, "simtesting.properties");
+
+                try {
+                        Properties props = new Properties();
+                        props.load(new FileInputStream(propsFile));
+                        return props;
+
+                } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                        throw new MissingPropertiesFile(propsFile);
+                }
+        }
+
+        public static String getProperty(String key) throws MissingProperty {
+
+                try {
+                        Properties props = loadProperties();
+                        return props.getProperty(key);
+                } catch (MissingPropertiesFile e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                        throw new MissingProperty(key);
+                }
+        }
+}
